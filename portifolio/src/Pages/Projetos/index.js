57d12769d2220de520b8a1366e26b6e9658ab4a1
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2022-06-03 16:05:36 -0300

    Corrigindo problemas

--- portifolio/src/Pages/Projetos/index.js
+++ portifolio/src/Pages/Projetos/index.js
@@ -9,13 +9,12 @@ function Projetos() {
                <div className='background-box-2 bg-green'></div>
                 <h1>Alguns Projetos</h1>
             <section className='container-project'>
-                <MiniCard imgOne={'./Assets/img/bike.gif'} linkOne={'https://prototypebikesgoods.netlify.app/'}>Site de Bike</MiniCard>
-                <MiniCard imgOne={'./Assets/img/doceria.gif'} linkOne={'https://protypeconfeitariajg.netlify.app/'}>Site Doceira</MiniCard>
-                <MiniCard imgOne={'./Assets/img/flor.gif'} linkOne={'https://protypefloriculturajg.netlify.app/'}>Site Floricultura</MiniCard>
-                <MiniCard imgOne={'./Assets/img/joaoaceso.gif'}>Sistema de Controle de acesso</MiniCard>
-                <MiniCard imgOne={'./Assets/img/landpage.png'}  linkOne={'https://jgpagina-de-captura.netlify.app'}>Pagina de Captura</MiniCard>
-                <MiniCard imgOne={'./Assets/img/oldsite.gif'} linkOne={'https://jgportifolio.netlify.app/'}>Site Intitucional</MiniCard>
-                <MiniCard imgOne={'./Assets/img/GifRegistPeople.gif'}>Sistema de cadastro de pessoas</MiniCard>
+                <MiniCard imgOne={'./Assets/img/bike.gif'} linkOne={'https://prototypebikesgoods.netlify.app/'}>Loja de Bicicletas</MiniCard>
+                <MiniCard imgOne={'./Assets/img/doceria.gif'} linkOne={'https://protypeconfeitariajg.netlify.app/'}>Uma Confeitaria</MiniCard>
+                <MiniCard imgOne={'./Assets/img/flor.gif'} linkOne={'https://protypefloriculturajg.netlify.app/'}>Uma floricultura</MiniCard>
+                <MiniCard imgOne={'./Assets/img/landpage.png'}  linkOne={'https://jgpagina-de-captura.netlify.app'}>Página de Captura</MiniCard>
+                <MiniCard imgOne={'./Assets/img/oldsite.gif'} linkOne={'https://jgportifolio.netlify.app/'}>Site Institucional</MiniCard>
+                <MiniCard imgOne={'./Assets/img/catalogo.gif'} linkOne={'https://protypelojademoveisjg.netlify.app'}>Loja de Móveis</MiniCard>
                 <MiniCard imgOne={'./Assets/img/CopaChat.gif'}>Chat Corporativo</MiniCard>
             </section>
         </section>)
