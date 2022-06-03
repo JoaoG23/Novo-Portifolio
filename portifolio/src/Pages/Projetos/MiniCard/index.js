15a78c0948992408bd67a5bd7f42967fce45a80b
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2022-06-03 11:06:17 -0300

    Linkando alguns projetos

--- portifolio/src/Pages/Projetos/MiniCard/index.js
+++ portifolio/src/Pages/Projetos/MiniCard/index.js
@@ -1,12 +1,20 @@
 import './style.css';
+import React from 'react';
+import { Link } from "react-router-dom";
 
-function MiniCard( props ) {
 
-    return(
-    <figure className='mini-card'>
-        <img className='img-mini-card' src={props.imgOne} alt='img'></img>
-        <figcaptiom className='nano-card'>{ props.children }</figcaptiom>
-    </figure>)
+function MiniCard(props) {
+
+    return (
+        <figure className='mini-card'>
+            <img className='img-mini-card' src={props.imgOne} alt='img'></img>
+            <a className='mask' href={props.linkOne}>
+                <div className='nano-card'>
+                    {props.children}
+                </div>
+            </a>
+        </figure>
+    )
 }
 
 export default MiniCard;
--- portifolio/src/Pages/Projetos/index.js
+++ portifolio/src/Pages/Projetos/index.js
@@ -4,19 +4,17 @@ import MiniCard from './MiniCard';
 function Projetos() {
     return(
         <section className='container-main-project transitionsSoft'>
-           <section>
                <div className='background-box-1 bg-red'></div>
                <div className='background-box-3 bg-yellow'></div>
                <div className='background-box-2 bg-green'></div>
-           </section>
                 <h1>Alguns Projetos</h1>
             <section className='container-project'>
-                <MiniCard imgOne={'./Assets/img/bike.gif'}>Site de Bike</MiniCard>
-                <MiniCard imgOne={'./Assets/img/doceria.gif'}>Site Doceira</MiniCard>
-                <MiniCard imgOne={'./Assets/img/flor.gif'}>Site Floricultura</MiniCard>
+                <MiniCard imgOne={'./Assets/img/bike.gif'} linkOne={'https://prototypebikesgoods.netlify.app/'}>Site de Bike</MiniCard>
+                <MiniCard imgOne={'./Assets/img/doceria.gif'} linkOne={'https://protypeconfeitariajg.netlify.app/'}>Site Doceira</MiniCard>
+                <MiniCard imgOne={'./Assets/img/flor.gif'} linkOne={'https://protypefloriculturajg.netlify.app/'}>Site Floricultura</MiniCard>
                 <MiniCard imgOne={'./Assets/img/joaoaceso.gif'}>Sistema de Controle de acesso</MiniCard>
                 <MiniCard imgOne={'./Assets/img/landpage.png'}>Pagina de Captura</MiniCard>
-                <MiniCard imgOne={'./Assets/img/oldsite.gif'}>Site Intitucional</MiniCard>
+                <MiniCard imgOne={'./Assets/img/oldsite.gif'} linkOne={'https://jgportifolio.netlify.app/'}>Site Intitucional</MiniCard>
                 <MiniCard imgOne={'./Assets/img/GifRegistPeople.gif'}>Sistema de cadastro de pessoas</MiniCard>
                 <MiniCard imgOne={'./Assets/img/CopaChat.gif'}>Chat Corporativo</MiniCard>
             </section>
